34f7dd2e29709d04476acebd6b166cc630e96ce0
Chris Richardson <91296586+CFRichardson@users.noreply.github.com>
2023-04-06 05:24:49 -0700

    Improved img_scope FN
    Simplified img_scope FN

--- func.py
+++ func.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  8 11:32:10 2023
+
+@author: christopherrichardson
+
+Some functions such as
+
+    return_chars
+
+should be broken down in full script to increase adjustability & repairability
+"""
+
 # In[Libraries]
 import cv2 as cv
 import numpy as np
@@ -21,58 +35,37 @@ def add_padding(img_, padding=20, r=255, g=255, b=255):
 
     if len(img_.shape) == 2:
         img_ = np.stack((img_,)*3, axis=-1)
-        
+
     old_image_height, old_image_width, channels = img_.shape
 
     new_height = old_image_height + padding
     new_width =  old_image_width + padding
-    
+
     result = np.full((new_height, new_width, channels), (r,g,b), dtype=np.uint8)
-    
+
     # compute center offset
     x_center = (new_width - old_image_width) // 2
     y_center = (new_height - old_image_height) // 2
-    
+
     # copy crop1 image into center of result image
-    result[y_center:y_center+old_image_height, 
+    result[y_center:y_center+old_image_height,
            x_center:x_center+old_image_width] = img_
-    
-    
+
+
     return result
 
 
-
-def img_2_str(img, config_int=7, special=''):
-    '''
-    https://pyimagesearch.com/2021/11/15/tesseract-page-segmentation-modes-psms-explained-how-to-improve-your-ocr-accuracy/
-    
-    Parameters
-    ----------
-    img : ___
-    
-    config_int : 
-        6: Assume a single uniform block of text
-        7: Single Line Text
-        12: Sparse Text
-        
-    Returns string from iamge
-    '''       
-    text = pytesseract.image_to_string(img, config=f'--psm {config_int} {special}')
-    return text.replace('\n', ' ')
-    
-
-
 def bgr_prep(img):
-    
+
     # all cropped keys have a trailing section with useless info
     rectangle = np.zeros((100,50), dtype='uint8')
     img = cv.rectangle(img, (350,150), (460,190), (255,255,255), -1)
-    
+
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     # threshold the image using Otsu's thresholding method
     thresh = cv.threshold(gray, 0, 255,
     	cv.THRESH_BINARY_INV | cv.THRESH_OTSU)[1]
-    
+
     # apply a distance transform which calculates the distance to the
     # closest zero pixel for each pixel in the input image
     dist = cv.distanceTransform(thresh, cv.DIST_L2, 5)
@@ -81,39 +74,116 @@ def bgr_prep(img):
     # an unsigned 8-bit integer in the range [0, 255]
     dist = cv.normalize(dist, dist, 0, 1.0, cv.NORM_MINMAX)
     dist = (dist * 255).astype("uint8")
-     
+
     # threshold the distance transform using Otsu's method
     dist = cv.threshold(dist, 0, 255,
     	cv.THRESH_BINARY | cv.THRESH_OTSU)[1]
-    
+
     dist = np.tile(dist[:, :, None], [1, 1, 3])
-    
+
     return dist
+
+
+def img_2_str(img, config_int=7, special=''):
+    '''
+    https://pyimagesearch.com/2021/11/15/tesseract-page-segmentation-modes-psms-explained-how-to-improve-your-ocr-accuracy/
+
+    Parameters
+    ----------
+    img : ___
+
+    config_int :
+        6: Assume a single uniform block of text
+        7: Single Line Text
+        12: Sparse Text
+
+    Returns string from iamge
+    '''
+    text = pytesseract.image_to_string(img, config=f'--psm {config_int} {special}')
+    return text.replace('\n', ' ')
+
+
+
+def img_scope(whole_img, x,y,w,h, x_adjust=0, y_adjust=0, bs=(350,120), be=(480,190)):
+    '''
+    All 3 strips (left, middle, and right) have different x_s, x_e, etc!
+    Parameters
+        ----------
+        whole_img : whole_scan
+
+
+
+        bs == block_start: starting point (y,x)
+        be == block_end: end point (y,x)
+
+        Returns
+        -------
+        scope: which is the specific area of wanted label strip containing KEYS
+
+    '''
+    x_s=390 + (x_adjust)
+    x_e=700 + (x_adjust)
+    y_s=80 + (y_adjust)
+    y_e=110 + (y_adjust)
+
+    scope = whole_img[y + y_s : y + h + y_e, x + x_s :x + w + x_e].copy()
+
+    # all cropped keys have a trailing section with useless info
+    scope = cv.rectangle(scope, bs,be, (0,0,0), -1)
+
+    return scope
+
+
 
 
 def mser_detect(img_):
     gray = cv.cvtColor(img_, cv.COLOR_BGR2GRAY)
-    
+
     chars = []
     canvas = np.zeros((200,500), dtype='uint8')
-    
+
     (h, w) = gray.shape[:2]
     image_size = h*w
     mser = cv.MSER_create()
     mser.setMaxArea(round(image_size/2))
     mser.setMinArea(2)
-    
+
     _, bw = cv.threshold(gray, 0.0, 255.0, cv.THRESH_OTSU )
-    
+
     regions, rects = mser.detectRegions(bw)
-    
+
     # With the rects you can e.g. crop the letters
     for (x, y, w, h) in rects:
         # cv.rectangle(gray, (x, y), (x+w, y+h), color=(255, 0, 255), thickness=1)
         if w > 15 and h >25:
             cv.rectangle(canvas, (x, y), (x+w, y+h), color=(255, 0, 255), thickness=1)
-    
+
     return canvas
+
+
+
+def return_mask(img_scope):
+    '''
+    Parameters
+        ----------
+        img_scope : specific area of label containing KEYS
+
+        Returns
+        -------
+        mask :  which is the text itself
+    '''
+
+    # Define lower and uppper limits for the color spectrum of white
+    lower = np.array([0, 0, 155])
+    upper = np.array([180, 40, 255])
+
+    imgHSV = cv.cvtColor(img_scope, cv.COLOR_BGR2HSV)
+    # create the Mask
+    mask = cv.inRange(imgHSV, lower, upper)
+    # inverse mask
+    mask = 255-mask
+
+    return mask
 
 
 def show(_img, cv_=True):
@@ -121,54 +191,37 @@ def show(_img, cv_=True):
         plt.imshow(cv.cvtColor(_img, cv.COLOR_BGR2RGB))
     else:
         plt.imshow(_img)
-        
+# ----------------------------------------------------------------------
+# In[ DEPRECATED ]
+# ----------------------------------------------------------------------
 
-
-def return_chars(whole_img, x,y,w,h, x_s=390, x_e=700, y_s=80, y_e=110):
-    
-    # the default x_s, x_e, y_s, y_e only works for single side scans!
-    
-    # middle and far right scans (sheet 2 & 3) reflect differently & thus
-    # a different quadrant of the Windows Logo is highlighted/
-    '''
-    Parameters
-        ----------
-        whole_img : whole_scan
-        
-        x & w: are starting points for the the lower left corner of Windows Logo
-        
-        wethen 
-   
-        Returns
-        -------
-        mask :  which is the text itself
-    '''     
-     
-    # calibrate for final version
-    scope = whole_img[y + y_s : y + h + y_e, x + x_s :x + w + x_e]
-    
-    # all cropped keys have a trailing section with useless info
-    rectangle = np.zeros((100,50), dtype='uint8')
-    scope = cv.rectangle(scope, (350,120), (480,190), (60,60,60), -1)
-    
-    # Define lower and uppper limits for the color spectrum of white
-    lower = np.array([0, 0, 155])
-    upper = np.array([180, 40, 255])
-    
-    imgHSV = cv.cvtColor(scope, cv.COLOR_BGR2HSV)
-    # create the Mask
-    mask = cv.inRange(imgHSV, lower, upper)
-    # inverse mask
-    mask = 255-mask
-    
-    return mask
-
-
-# In[]
-
-
-
-
-
-
-
+# def return_chars(whole_img, x,y,w,h, x_s=410, x_e=730, y_s=90, y_e=110):
+#     '''
+#     Parameters
+#         ----------
+#         whole_img : whole_scan
+#         x & w: are starting points for the the lower left corner of Windows Logo
+#
+#         Returns
+#         -------
+#         mask :  which is the text itself
+#     '''
+#
+#     # calibrate for final version
+#     scope = whole_img[y + y_s : y + h + y_e, x + x_s :x + w + x_e]
+#
+#     # all cropped keys have a trailing section with useless info
+#     rectangle = np.zeros((100,50), dtype='uint8')
+#     scope = cv.rectangle(scope, (350,150), (460,190), (60,60,60), -1)
+#
+#     # Define lower and uppper limits for the color spectrum of white
+#     lower = np.array([0, 0, 155])
+#     upper = np.array([180, 40, 255])
+#
+#     imgHSV = cv.cvtColor(scope, cv.COLOR_BGR2HSV)
+#     # create the Mask
+#     mask = cv.inRange(imgHSV, lower, upper)
+#     # inverse mask
+#     mask = 255-mask
+#
+#     return mask
